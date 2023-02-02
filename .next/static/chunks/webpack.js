1a8aa56805c678c2f17102daaf672adb04bcb747
MarcosCast <marcosmax.5@gmail.com>
2023-02-02 15:08:57 -0300

    Erro API

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5b5519380a3a0c7"; }
+/******/ 		__webpack_require__.h = function() { return "cbf311d83ddd0624"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
