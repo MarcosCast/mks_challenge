3fa398bb8cb8aa974a9348f35cda8b32004396ae
MarcosCast <marcosmax.5@gmail.com>
2023-02-02 13:23:19 -0300

    Estruturando a página

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+import { Main } from 'next/document'
 
 const Home = () => {
     return (
-        <div>
-            <p>MKS Challenge - 3001</p>
-        </div>
+        
+        <PageContainer>
+
+            <NavBar>
+                <Link>
+                   <Logo>
+                        MKS
+                   </Logo>
+                </Link>
+                <Link>
+                  <ButtonNav>
+
+                  </ButtonNav>
+                </Link>
+
+            </NavBar>
+
+            <Main>
+
+                <Headline>
+                    Produtos
+                </Headline>
+                <SubHeadLine>
+                    EM BREVE!
+                </SubHeadLine>
+
+                <MainActions>
+                    <Link>
+                        <ButtonNav> Acesse </ButtonNav>
+                    </Link>
+                    <Link>
+                       <ButtonNav> Saiba Mais </ButtonNav>
+                    </Link>
+                </MainActions>
+
+            </Main>
+
+        </PageContainer>
     )
 }
 
