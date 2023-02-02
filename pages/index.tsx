bddb2e2609a4fd53199984d5346d37693d4379ef
MarcosCast <marcosmax.5@gmail.com>
2023-02-02 15:08:39 -0300

    Erro na hora de retornar a api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
+import axios from 'axios';
 
 const Home = () => {
-    return (
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+        const url = 'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=6&sortBy=id&orderBy=ASC';
+        const response = await fetch(url);
+        const objJson = await response.json()
+        setData(objJson. Resultados)
+    }
+    fetchApi();
+    },[])
+
+    return(
+        <div>
+            <h1>MKS</h1>
+            <div>
+                {
+                    data.map((e) => (
+                        <div key={e.id}>
+                          <h4>{e.name}</h4>
+                          <img src={e.photo} alt="" />
+                          <h4>{`R$ ${e.price}`}</h4>
+                          </div>
+                    ))      
+                }            
+            </div>
+
+        </div>
         
-        <PageContainer>
-            <NavBar>
-                <Link href='/'>
-                   <Logo>
-                        MKS
-                   </Logo>
-                   <SubLogo>
-                        Sistemas
-                   </SubLogo>
-                </Link>
-                <Link href='/'>
-                  <ButtonNav>
-
-                  </ButtonNav>
-                </Link>
-            </NavBar>
-
-            <Main>
-                <Headline>
-                    Produtos
-                </Headline>
-                <SubHeadLine>
-                    EM BREVE!
-                </SubHeadLine>
-
-                <MainActions>
-                </MainActions>
-            </Main>
-        </PageContainer>
+        
     )
 }
 
-export default Home
-
-export const PageContainer = styled.div``
-export const NavBar = styled.div`
-
-        height: 101px;
-        width: 1440px;
-        left: 0px;
-        top: 0px;
-        border-radius: 0px;
-
-        background: #0F52BA;
-
-
-`
-export const Logo = styled.p`
-        height: 44px;
-        width: 128px;
-        left: 65px;
-        top: 28px;
-        border-radius: nullpx;
-
-        font-family: 'Montserrat', sans-serif;
-        font-size: 40px;
-        font-weight: 600;
-        line-height: 19px;
-        letter-spacing: 0px;
-        text-align: left;
-
-
-`
-
-export const SubLogo = styled.p``
-
-export const Main = styled.main``
-export const Headline = styled.h1``
-export const SubHeadLine = styled.h2``
-export const MainActions = styled.div``
-export const Button = styled.button``
-export const ButtonNav = styled(Button)``
-export const ButtonActions = styled(Button)``
-
+export default Home;
+    
+    
+    
